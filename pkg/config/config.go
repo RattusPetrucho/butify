c4446f9497d2c2322f9eeeb248527e31c0c214f0
RattusPetrucho <rattuspetrucho@protonmail.com>
2021-06-18 14:39:02 +0300

    Add DB config

---
+++ pkg/config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import "os"
+
+type Cfg struct {
+	DbDsn     string
+	DbDialect string
+}
+
+func New() Cfg {
+	return Cfg{
+		DbDsn:     readFromEnv("DB_DSN", "postgresql://postgres:postgres@localhost:5432/butify?sslmode=disable"),
+		DbDialect: readFromEnv("DB_DIALECT", "postgres"),
+	}
+}
+
+func readFromEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		value = defaultValue
+	}
+
+	return value
+}
